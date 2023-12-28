24e6f2b4b6b1d6d67086b7ad7702abd2c2872ad5
ANNU KUMARI <anuk35168@gmail.com>
2023-12-28 16:39:30 +0530

    commit changes

---
+++ script.js
@@ -0,0 +1,182 @@
+searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+}
+
+let loginform = document.querySelector('.login-form-container');
+
+document.querySelector('#login-btn').onclick = () => {
+    loginform.classList.toggle('active')
+}
+
+document.querySelector('#close-login-btn').onclick = () => {
+    loginform.classList.remove('active');
+}
+
+window.onscroll = () => {
+
+    searchForm.classList.remove('active');
+
+    if (window.scrollY > 80) {
+        document.querySelector('.header .header-2').classList.add('active');
+    }
+    else {
+        document.querySelector('.header .header-2').classList.remove('active');
+    }
+
+}
+
+fadeOut();
+
+function loader(){
+    document.querySelector(".loader-container").classList.add("active");
+}
+
+function fadeOut(){
+   setTimeout(loader, 5000);
+}
+
+window.onload = () => {
+
+    if (window.scrollY > 80) {
+        document.querySelector('.header .header-2').classList.add('active');
+    }
+    else {
+        document.querySelector('.header .header-2').classList.remove('active');
+    }
+}
+
+var swiper = new Swiper(".gifts-slider", {
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+
+
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+
+        },
+        768: {
+            slidesPerView: 2,
+
+        },
+        1024: {
+            slidesPerView: 3,
+
+        },
+    },
+});
+
+
+
+var swiper = new Swiper(".featured-slider", {
+    spaceBetween:10,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+
+ },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+
+   
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+
+        },
+
+        450: {
+            slidesPerView: 2,
+
+        },
+        768: {
+            slidesPerView: 3,
+
+        },
+        1024: {
+            slidesPerView: 4,
+
+        },
+    },
+});
+
+
+
+var swiper = new Swiper(".reviews-slider", {
+    spaceBetween:10,
+    grabcursor:true,
+
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+
+
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+
+        },
+        768: {
+            slidesPerView: 2,
+
+        },
+        1024: {
+            slidesPerView: 3,
+
+        },
+    },
+});
+
+
+var swiper = new Swiper(".pqr-slider", {
+    spaceBetween:10,
+    grabcursor:true,
+
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+
+
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+
+        },
+       
+    
+    768: {
+        slidesPerView: 2,
+
+    },
+    1024: {
+        slidesPerView: 3,
+
+    },
+},
+});
